5f82649e49cdd30d8a9da12b2cd28b1b1cf792dd
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-12-10 23:04:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 data "aws_caller_identity" "current" {}
 
-module "networking" {
-  source           = "./modules/networking"
+# module "networking" {
+#   source           = "./modules/networking"
  
-}
+# }
 
 module "ecr" {
   source = "./modules/ecr"
@@ -12,59 +12,59 @@ module "ecr" {
 module "ecs" {
   source                 = "./modules/ecs"
   cluster_name           = "dotnet-app-cluster"
-  vpc_id                 = module.networking.vpc_id
-  subnet_ids             = module.networking.subnet_ids
+  vpc_id                 = ""
+  subnet_ids             = ["",""]
   container_image        = "${module.ecr.ecr_repository_url}:latest"
-  security_group_ids     = [aws_security_group.ecs_service.id]
-  alb_target_group_arn   = module.alb.target_group_arn
+  security_group_ids     = [""]
+  alb_target_group_arn   = null
 
 }
 
-resource "aws_security_group" "ecs_service" {
-  name        = "ecs-service-sg"
-  description = "Security group for ECS service"
-  vpc_id      = module.networking.vpc_id
+# resource "aws_security_group" "ecs_service" {
+#   name        = "ecs-service-sg"
+#   description = "Security group for ECS service"
+#   vpc_id      = module.networking.vpc_id
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-   security_groups = [aws_security_group.alb.id]
-  }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#    security_groups = [aws_security_group.alb.id]
+#   }
  
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-resource "aws_security_group" "alb" {
-  name        = "alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = module.networking.vpc_id
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+# resource "aws_security_group" "alb" {
+#   name        = "alb-sg"
+#   description = "Security group for ALB"
+#   vpc_id      = module.networking.vpc_id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allows HTTP traffic from the internet
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allows HTTP traffic from the internet
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-module "alb" {
-  source                = "./modules/alb"
-  vpc_id                = module.networking.vpc_id
-  subnet_ids            = module.networking.subnet_ids
-  security_group_id     = [aws_security_group.alb.id]
-  ecs_service_private_ips = module.ecs.private_ips  
-}
+# module "alb" {
+#   source                = "./modules/alb"
+#   vpc_id                = module.networking.vpc_id
+#   subnet_ids            = module.networking.subnet_ids
+#   security_group_id     = [aws_security_group.alb.id]
+#   ecs_service_private_ips = module.ecs.private_ips  # Ensure this outputs ECS private IPs
+# }
 
